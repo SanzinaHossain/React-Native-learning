13d6886e59a14425f9f3c1042931ed1b625d252d
SanzinaHossain <sanzina67hossain@gmail.com>
2022-11-27 11:38:29 +0600

    react-native-initial

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.bo}>
+      <Text>My first React Native App</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+    alignItems: "center",
+  },
+  bo: {
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    flex: 1,
   },
 });
